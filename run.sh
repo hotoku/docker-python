10108892fd5915e4bad4b8e7704fea959a526bfe
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-12-04 17:05:22 +0900

    portを指定できるように

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #/bin/bash
 
-docker run -it --rm -p 8888:8888 -v /Users/hotoku/project/ml_lesson:/root/work hotoku/python
+PORT=8888
+VOLUME=/Users/hotoku/samples/python
 
+while getopts p:v: OPT; do
+    case ${OPT} in
+        p) PORT=${OPTARG}   ;;
+        v) VOLUME=${OPTARG} ;;
+        *) exit 1           ;;
+    esac
+done
+
+
+docker run -d --rm -p ${PORT}:8888 -v ${VOLUME}:/root/work hotoku/python bash -lc /usr/local/bin/run-lab

de2500e38bb74e2bcd9e1aa001e88f3a1643766e
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2019-12-24 22:46:48 +0900

    fix

---
+++ jupyterlab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+cat /tmp/plist-jupyter.err                       |\
+    grep http://127                              |\
+    tail -n1                                     |\
+    awk '{print $2}'                             |\
+    sed -E -e "s|127.0.0.1:8888|127.0.0.1:9002|" |\
+    xargs open
